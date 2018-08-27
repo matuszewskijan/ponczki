a34ee53ff48732b691e06081ab758a7c0ec02b22
Jan Matuszewski <luirun@gazeta.pl>
2018-08-27 12:03:48 +0200

    Send right argument to User#add_team

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def slack
     @user = User.from_omniauth(request.env["omniauth.auth"])
     @team = Team.from_omniauth(request.env["omniauth.auth"])
-    @user.add_team(@team.slack_id)
+    @user.add_team(@team.id)
     sign_in @user
     redirect_to become_team_admin_path(@team.slack_name) if @team.admin.nil?
     redirect_to user_path(@user.slack_nick) unless @team.admin.nil?
